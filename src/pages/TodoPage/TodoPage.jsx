c5dbf2c68d828118d858a919084ca2b18f350dc4
Mostafa-YMS <mos_302@yahoo.com>
2024-07-27 14:15:52 +0300

    load more button

--- src/pages/TodoPage/TodoPage.jsx
+++ src/pages/TodoPage/TodoPage.jsx
@@ -22,19 +22,11 @@ const TodoPage = () => {
     }
   }, []);
 
-  const onScroll = useCallback(
-    (e) => {
-      if (loading) return;
-      const { scrollHeight, scrollTop, clientHeight } = e.target;
-      if (
-        scrollTop + clientHeight + 50 >= scrollHeight &&
-        pagination["hasMore"] === true
-      ) {
-        getTodo(pagination.page + 1);
-      }
-    },
-    [pagination, loading, getTodo]
-  );
+  const loadMore = useCallback(() => {
+    if (pagination?.hasMore && !loading) {
+      getTodo(pagination.page + 1);
+    }
+  }, [pagination, loading, getTodo]);
 
   useEffect(() => {
     getTodo && getTodo();
@@ -65,12 +57,20 @@ const TodoPage = () => {
   );
 
   return (
-    <div className={styles.container} onScroll={onScroll}>
+    <div className={styles.container}>
       <div className={styles.addBtnContainer}>
         <AddTodo onAddTodo={onAddTodo} />
       </div>
       <TodoList todos={todos} onToggle={onToggle} onDelete={onDelete} />
-      {loading && <div>Loading...</div>}
+      <div className={styles.addBtnContainer}>
+        <button
+          className={styles.loadMoreButton}
+          onClick={loadMore}
+          disabled={loading || !pagination?.hasMore}
+        >
+          {loading ? "Loading..." : "Load More"}
+        </button>
+      </div>
     </div>
   );
 };
